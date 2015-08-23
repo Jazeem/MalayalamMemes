9715916fc6dee801aa42e716cf93e8fefd128aff
Jazeem <jazeem23@gmail.com>
2015-08-23 21:57:21 +0530

    initial commit

---
+++ app/src/main/java/com/clusterdev/malayalammemes/malayalammemes/MainActivity.java
@@ -0,0 +1,205 @@
+package com.clusterdev.malayalammemes.malayalammemes;
+
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.AsyncTask;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.entity.BufferedHttpEntity;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.util.EntityUtils;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+
+public class MainActivity extends ActionBarActivity {
+
+    private TextView tv;
+    private ImageView img;
+    private String PostID = null;
+    private String PhotoID = null;
+    private String baseUrl = "https://graph.facebook.com/v2.4/";
+    private String OAuth = "151023885236941|y1tgdIybKDV1JD6etX0AUehPFF0";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        tv = (TextView)findViewById(R.id.textview);
+        img = (ImageView)findViewById(R.id.img_view);
+        new RequestPost().execute();
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+    class RequestPost extends AsyncTask<String, String, String> {
+
+        @Override
+        protected String doInBackground(String... uri) {
+            HttpClient httpclient = new DefaultHttpClient();
+            HttpResponse response;
+            String responseString = null;
+
+
+            HttpGet httpGet = new HttpGet(baseUrl+"internationalchaluunion/posts");
+            httpGet.setHeader("Authorization", "OAuth "+OAuth);
+            try {
+                response = httpclient.execute(httpGet);
+                JSONObject json = new JSONObject(EntityUtils.toString(response.getEntity()));
+                JSONArray data = (JSONArray) json.get("data");
+
+                responseString =  data.getJSONObject(0).get("id").toString();
+
+                Log.v("id", responseString);
+            } catch (IOException e) {
+                e.printStackTrace();
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+
+
+            return responseString;
+        }
+
+        @Override
+        protected void onPostExecute(String result) {
+            super.onPostExecute(result);
+            //Do anything with response..
+
+
+            PostID = result;
+            new RequestID().execute();
+
+        }
+    }
+    class RequestID extends AsyncTask<String, String, String> {
+
+        @Override
+        protected String doInBackground(String... uri) {
+            HttpClient httpclient = new DefaultHttpClient();
+            HttpResponse response;
+            String responseString = null;
+
+            String baseUrl = "https://graph.facebook.com/v2.4/";
+            HttpGet httpGet;
+            httpGet = new HttpGet(baseUrl+PostID+"?fields=object_id");
+            httpGet.setHeader("Authorization", "OAuth "+OAuth);
+            try {
+                response = httpclient.execute(httpGet);
+                JSONObject json = new JSONObject(EntityUtils.toString(response.getEntity()));
+                responseString = json.get("object_id").toString();
+                Log.v("object_id", responseString);
+            } catch (IOException e) {
+                e.printStackTrace();
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+
+
+            return responseString;
+        }
+
+        @Override
+        protected void onPostExecute(String result) {
+            super.onPostExecute(result);
+            //Do anything with response..
+            PhotoID = result;
+            new RequestImageURL().execute();
+
+        }
+    }
+    class RequestImageURL extends AsyncTask<String, String, Bitmap> {
+
+        @Override
+        protected Bitmap doInBackground(String... uri) {
+            HttpClient httpclient = new DefaultHttpClient();
+            HttpResponse response;
+            String responseString = null;
+            Bitmap bmp = null;
+            String baseUrl = "https://graph.facebook.com/v2.4/";
+            HttpGet httpGet;
+            httpGet = new HttpGet(baseUrl+PhotoID+"/picture");
+            httpGet.setHeader("Authorization", "OAuth "+OAuth);
+            try {
+                response = httpclient.execute(httpGet);
+                Log.v("url.raw", response.toString());
+                //JSONObject json = new JSONObject(EntityUtils.toString(response.getEntity()));
+
+//                responseString = json.getJSONObject("data").get("url").toString();
+//                Log.v("url", responseString);
+
+                String entityContents="";
+                HttpEntity responseEntity = response.getEntity();
+                BufferedHttpEntity httpEntity = null;
+                try {
+                    httpEntity = new BufferedHttpEntity(responseEntity);
+                } catch (IOException e1) {
+                    // TODO Auto-generated catch block
+                    e1.printStackTrace();
+                }
+                InputStream imageStream = null;
+                try {
+                    imageStream = httpEntity.getContent();
+                } catch (IOException e) {
+                    // TODO Auto-generated catch block
+                    e.printStackTrace();
+                }
+                bmp = BitmapFactory.decodeStream(imageStream);
+
+
+
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+
+            return bmp;
+        }
+
+        @Override
+        protected void onPostExecute(Bitmap result) {
+            super.onPostExecute(result);
+            //Do anything with response..
+
+            img.setImageBitmap(result);
+
+
+        }
+    }
+}
+
